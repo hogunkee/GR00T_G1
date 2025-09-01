f7dd210c1a2a2e638d4083f0af27a195e9e337a6
hogun-pikachu <hogun.kee@rllab.snu.ac.kr>
2025-09-01 17:46:55 +0900

    g1 finetuning

--- run_eval_g1.sh
+++ run_eval_g1.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 TAG=$1
-./scripts/simul_eval_g1.sh 0 g1_unified/PnPCupToDrawerClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
-./scripts/simul_eval_g1.sh 0 g1_unified/PnPPotatoToMicrowaveClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
-./scripts/simul_eval_g1.sh 0 g1_unified/PnPMilkToMicrowaveClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
-./scripts/simul_eval_g1.sh 0 g1_unified/PnPBottleToCabinetClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
-./scripts/simul_eval_g1.sh 0 g1_unified/PnPWineToCabinetClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
-./scripts/simul_eval_g1.sh 0 g1_unified/PnPCanToDrawerClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
 ./scripts/simul_eval_g1.sh 0 g1_unified/PosttrainPnPNovelFromCuttingboardToBasketSplitA_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
 ./scripts/simul_eval_g1.sh 0 g1_unified/PosttrainPnPNovelFromCuttingboardToCardboardboxSplitA_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
 ./scripts/simul_eval_g1.sh 0 g1_unified/PosttrainPnPNovelFromCuttingboardToPanSplitA_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
@@ -23,5 +17,10 @@ TAG=$1
 ./scripts/simul_eval_g1.sh 0 g1_unified/PosttrainPnPNovelFromTrayToPlateSplitA_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
 ./scripts/simul_eval_g1.sh 0 g1_unified/PosttrainPnPNovelFromTrayToPotSplitA_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
 ./scripts/simul_eval_g1.sh 0 g1_unified/PosttrainPnPNovelFromTrayToTieredbasketSplitA_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
-./scripts/simul_eval_g1.sh 0 g1_unified/PosttrainPnPNovelFromTrayToTieredshelfSplitA_G1ArmsAndWaistDex31Hands_Env ${TAG}
-
+./scripts/simul_eval_g1.sh 0 g1_unified/PosttrainPnPNovelFromTrayToTieredshelfSplitA_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
+./scripts/simul_eval_g1.sh 0 g1_unified/PnPCupToDrawerClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
+./scripts/simul_eval_g1.sh 0 g1_unified/PnPPotatoToMicrowaveClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
+./scripts/simul_eval_g1.sh 0 g1_unified/PnPMilkToMicrowaveClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
+./scripts/simul_eval_g1.sh 0 g1_unified/PnPBottleToCabinetClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
+./scripts/simul_eval_g1.sh 0 g1_unified/PnPWineToCabinetClose_G1ArmsAndWaistDex31Hands_Env ${TAG} &&
+./scripts/simul_eval_g1.sh 0 g1_unified/PnPCanToDrawerClose_G1ArmsAndWaistDex31Hands_Env ${TAG}
--- scripts/simul_eval_g1.sh
+++ scripts/simul_eval_g1.sh
@@ -4,4 +4,4 @@ GPU_ID=$1
 TASK_NAME=$2
 TAG=$3
 
-CUDA_VISIBLE_DEVICES=${GPU_ID} python scripts/simulation_service.py --client --env_name ${TASK_NAME} --video_dir ./videos/${TAG}/${TASK_NAME} --max_episode_steps 360 --n_envs 5 --n_episodes 10 --log --multi_video --embodiment_tag g1
+CUDA_VISIBLE_DEVICES=${GPU_ID} python scripts/simulation_service.py --client --env_name ${TASK_NAME} --video_dir ./videos/${TAG}/${TASK_NAME} --max_episode_steps 360 --n_envs 5 --n_episodes 5 --log --multi_video --embodiment_tag g1
